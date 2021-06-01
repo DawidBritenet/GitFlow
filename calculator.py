1242794ab064352680478d19107635accb171832
unknown <dawid.surma@britenet.com.pl>
2021-06-01 09:25:43 +0200

    Add-Subtract-Function

--- calculator.py
+++ calculator.py
@@ -246,3 +246,245 @@ if num1 == 10 and sign == '+' and num2 == 9:
     print("10+9 = 19")
 if num1 == 10 and sign == '+' and num2 == 10:
     print("10+10 = 20")
+if num1 == 0 and sign == '-' and num2 == 0:
+    print("0-0 = 0")
+if num1 == 0 and sign == '-' and num2 == 1:
+    print("0-1 = -1")
+if num1 == 0 and sign == '-' and num2 == 2:
+    print("0-2 = -2")
+if num1 == 0 and sign == '-' and num2 == 3:
+    print("0-3 = -3")
+if num1 == 0 and sign == '-' and num2 == 4:
+    print("0-4 = -4")
+if num1 == 0 and sign == '-' and num2 == 5:
+    print("0-5 = -5")
+if num1 == 0 and sign == '-' and num2 == 6:
+    print("0-6 = -6")
+if num1 == 0 and sign == '-' and num2 == 7:
+    print("0-7 = -7")
+if num1 == 0 and sign == '-' and num2 == 8:
+    print("0-8 = -8")
+if num1 == 0 and sign == '-' and num2 == 9:
+    print("0-9 = -9")
+if num1 == 0 and sign == '-' and num2 == 10:
+    print("0-10 = -10")
+if num1 == 1 and sign == '-' and num2 == 0:
+    print("1-0 = 1")
+if num1 == 1 and sign == '-' and num2 == 1:
+    print("1-1 = 0")
+if num1 == 1 and sign == '-' and num2 == 2:
+    print("1-2 = -1")
+if num1 == 1 and sign == '-' and num2 == 3:
+    print("1-3 = -2")
+if num1 == 1 and sign == '-' and num2 == 4:
+    print("1-4 = -3")
+if num1 == 1 and sign == '-' and num2 == 5:
+    print("1-5 = -4")
+if num1 == 1 and sign == '-' and num2 == 6:
+    print("1-6 = -5")
+if num1 == 1 and sign == '-' and num2 == 7:
+    print("1-7 = -6")
+if num1 == 1 and sign == '-' and num2 == 8:
+    print("1-8 = -7")
+if num1 == 1 and sign == '-' and num2 == 9:
+    print("1-9 = -8")
+if num1 == 1 and sign == '-' and num2 == 10:
+    print("1-10 = -9")
+if num1 == 2 and sign == '-' and num2 == 0:
+    print("2-0 = 2")
+if num1 == 2 and sign == '-' and num2 == 1:
+    print("2-1 = 1")
+if num1 == 2 and sign == '-' and num2 == 2:
+    print("2-2 = 0")
+if num1 == 2 and sign == '-' and num2 == 3:
+    print("2-3 = -1")
+if num1 == 2 and sign == '-' and num2 == 4:
+    print("2-4 = -2")
+if num1 == 2 and sign == '-' and num2 == 5:
+    print("2-5 = -3")
+if num1 == 2 and sign == '-' and num2 == 6:
+    print("2-6 = -4")
+if num1 == 2 and sign == '-' and num2 == 7:
+    print("2-7 = -5")
+if num1 == 2 and sign == '-' and num2 == 8:
+    print("2-8 = -6")
+if num1 == 2 and sign == '-' and num2 == 9:
+    print("2-9 = -7")
+if num1 == 2 and sign == '-' and num2 == 10:
+    print("2-10 = -8")
+if num1 == 3 and sign == '-' and num2 == 0:
+    print("3-0 = 3")
+if num1 == 3 and sign == '-' and num2 == 1:
+    print("3-1 = 2")
+if num1 == 3 and sign == '-' and num2 == 2:
+    print("3-2 = 1")
+if num1 == 3 and sign == '-' and num2 == 3:
+    print("3-3 = 0")
+if num1 == 3 and sign == '-' and num2 == 4:
+    print("3-4 = -1")
+if num1 == 3 and sign == '-' and num2 == 5:
+    print("3-5 = -2")
+if num1 == 3 and sign == '-' and num2 == 6:
+    print("3-6 = -3")
+if num1 == 3 and sign == '-' and num2 == 7:
+    print("3-7 = -4")
+if num1 == 3 and sign == '-' and num2 == 8:
+    print("3-8 = -5")
+if num1 == 3 and sign == '-' and num2 == 9:
+    print("3-9 = -6")
+if num1 == 3 and sign == '-' and num2 == 10:
+    print("3-10 = -7")
+if num1 == 4 and sign == '-' and num2 == 0:
+    print("4-0 = 4")
+if num1 == 4 and sign == '-' and num2 == 1:
+    print("4-1 = 3")
+if num1 == 4 and sign == '-' and num2 == 2:
+    print("4-2 = 2")
+if num1 == 4 and sign == '-' and num2 == 3:
+    print("4-3 = 1")
+if num1 == 4 and sign == '-' and num2 == 4:
+    print("4-4 = 0")
+if num1 == 4 and sign == '-' and num2 == 5:
+    print("4-5 = -1")
+if num1 == 4 and sign == '-' and num2 == 6:
+    print("4-6 = -2")
+if num1 == 4 and sign == '-' and num2 == 7:
+    print("4-7 = -3")
+if num1 == 4 and sign == '-' and num2 == 8:
+    print("4-8 = -4")
+if num1 == 4 and sign == '-' and num2 == 9:
+    print("4-9 = -5")
+if num1 == 4 and sign == '-' and num2 == 10:
+    print("4-10 = -6")
+if num1 == 5 and sign == '-' and num2 == 0:
+    print("5-0 = 5")
+if num1 == 5 and sign == '-' and num2 == 1:
+    print("5-1 = 4")
+if num1 == 5 and sign == '-' and num2 == 2:
+    print("5-2 = 3")
+if num1 == 5 and sign == '-' and num2 == 3:
+    print("5-3 = 2")
+if num1 == 5 and sign == '-' and num2 == 4:
+    print("5-4 = 1")
+if num1 == 5 and sign == '-' and num2 == 5:
+    print("5-5 = 0")
+if num1 == 5 and sign == '-' and num2 == 6:
+    print("5-6 = -1")
+if num1 == 5 and sign == '-' and num2 == 7:
+    print("5-7 = -2")
+if num1 == 5 and sign == '-' and num2 == 8:
+    print("5-8 = -3")
+if num1 == 5 and sign == '-' and num2 == 9:
+    print("5-9 = -4")
+if num1 == 5 and sign == '-' and num2 == 10:
+    print("5-10 = -5")
+if num1 == 6 and sign == '-' and num2 == 0:
+    print("6-0 = 6")
+if num1 == 6 and sign == '-' and num2 == 1:
+    print("6-1 = 5")
+if num1 == 6 and sign == '-' and num2 == 2:
+    print("6-2 = 4")
+if num1 == 6 and sign == '-' and num2 == 3:
+    print("6-3 = 3")
+if num1 == 6 and sign == '-' and num2 == 4:
+    print("6-4 = 2")
+if num1 == 6 and sign == '-' and num2 == 5:
+    print("6-5 = 1")
+if num1 == 6 and sign == '-' and num2 == 6:
+    print("6-6 = 0")
+if num1 == 6 and sign == '-' and num2 == 7:
+    print("6-7 = -1")
+if num1 == 6 and sign == '-' and num2 == 8:
+    print("6-8 = -2")
+if num1 == 6 and sign == '-' and num2 == 9:
+    print("6-9 = -3")
+if num1 == 6 and sign == '-' and num2 == 10:
+    print("6-10 = -4")
+if num1 == 7 and sign == '-' and num2 == 0:
+    print("7-0 = 7")
+if num1 == 7 and sign == '-' and num2 == 1:
+    print("7-1 = 6")
+if num1 == 7 and sign == '-' and num2 == 2:
+    print("7-2 = 5")
+if num1 == 7 and sign == '-' and num2 == 3:
+    print("7-3 = 4")
+if num1 == 7 and sign == '-' and num2 == 4:
+    print("7-4 = 3")
+if num1 == 7 and sign == '-' and num2 == 5:
+    print("7-5 = 2")
+if num1 == 7 and sign == '-' and num2 == 6:
+    print("7-6 = 1")
+if num1 == 7 and sign == '-' and num2 == 7:
+    print("7-7 = 0")
+if num1 == 7 and sign == '-' and num2 == 8:
+    print("7-8 = -1")
+if num1 == 7 and sign == '-' and num2 == 9:
+    print("7-9 = -2")
+if num1 == 7 and sign == '-' and num2 == 10:
+    print("7-10 = -3")
+if num1 == 8 and sign == '-' and num2 == 0:
+    print("8-0 = 8")
+if num1 == 8 and sign == '-' and num2 == 1:
+    print("8-1 = 7")
+if num1 == 8 and sign == '-' and num2 == 2:
+    print("8-2 = 6")
+if num1 == 8 and sign == '-' and num2 == 3:
+    print("8-3 = 5")
+if num1 == 8 and sign == '-' and num2 == 4:
+    print("8-4 = 4")
+if num1 == 8 and sign == '-' and num2 == 5:
+    print("8-5 = 3")
+if num1 == 8 and sign == '-' and num2 == 6:
+    print("8-6 = 2")
+if num1 == 8 and sign == '-' and num2 == 7:
+    print("8-7 = 1")
+if num1 == 8 and sign == '-' and num2 == 8:
+    print("8-8 = 0")
+if num1 == 8 and sign == '-' and num2 == 9:
+    print("8-9 = -1")
+if num1 == 8 and sign == '-' and num2 == 10:
+    print("8-10 = -2")
+if num1 == 9 and sign == '-' and num2 == 0:
+    print("9-0 = 9")
+if num1 == 9 and sign == '-' and num2 == 1:
+    print("9-1 = 8")
+if num1 == 9 and sign == '-' and num2 == 2:
+    print("9-2 = 7")
+if num1 == 9 and sign == '-' and num2 == 3:
+    print("9-3 = 6")
+if num1 == 9 and sign == '-' and num2 == 4:
+    print("9-4 = 5")
+if num1 == 9 and sign == '-' and num2 == 5:
+    print("9-5 = 4")
+if num1 == 9 and sign == '-' and num2 == 6:
+    print("9-6 = 3")
+if num1 == 9 and sign == '-' and num2 == 7:
+    print("9-7 = 2")
+if num1 == 9 and sign == '-' and num2 == 8:
+    print("9-8 = 1")
+if num1 == 9 and sign == '-' and num2 == 9:
+    print("9-9 = 0")
+if num1 == 9 and sign == '-' and num2 == 10:
+    print("9-10 = -1")
+if num1 == 10 and sign == '-' and num2 == 0:
+    print("10-0 = 10")
+if num1 == 10 and sign == '-' and num2 == 1:
+    print("10-1 = 9")
+if num1 == 10 and sign == '-' and num2 == 2:
+    print("10-2 = 8")
+if num1 == 10 and sign == '-' and num2 == 3:
+    print("10-3 = 7")
+if num1 == 10 and sign == '-' and num2 == 4:
+    print("10-4 = 6")
+if num1 == 10 and sign == '-' and num2 == 5:
+    print("10-5 = 5")
+if num1 == 10 and sign == '-' and num2 == 6:
+    print("10-6 = 4")
+if num1 == 10 and sign == '-' and num2 == 7:
+    print("10-7 = 3")
+if num1 == 10 and sign == '-' and num2 == 8:
+    print("10-8 = 2")
+if num1 == 10 and sign == '-' and num2 == 9:
+    print("10-9 = 1")
+if num1 == 10 and sign == '-' and num2 == 10:
+    print("10-10 = 0")
