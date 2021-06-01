ee5f7d54aff69f0bbf75ea3a142c77c5f8632f7a
unknown <dawid.surma@britenet.com.pl>
2021-06-01 09:12:53 +0200

    Add-Add-Function

--- calculator.py
+++ calculator.py
@@ -1,5 +1,248 @@
-print('Welcome to this supreme calculator!')
-print('It can add, subtract, multiply and divide whole numbers from 0 to 50')
+print('Welcome to this calculator!')
+print('It can add, subtract, multiply and divide whole numbers from 0 to 10')
 num1 = int(input('Please choose your first number: '))
 sign = input('What do you want to do? +, -, /, or *: ')
 num2 = int(input('Please choose your second number: '))
+
+if num1 == 0 and sign == '+' and num2 == 0:
+    print("0+0 = 0")
+if num1 == 0 and sign == '+' and num2 == 1:
+    print("0+1 = 1")
+if num1 == 0 and sign == '+' and num2 == 2:
+    print("0+2 = 2")
+if num1 == 0 and sign == '+' and num2 == 3:
+    print("0+3 = 3")
+if num1 == 0 and sign == '+' and num2 == 4:
+    print("0+4 = 4")
+if num1 == 0 and sign == '+' and num2 == 5:
+    print("0+5 = 5")
+if num1 == 0 and sign == '+' and num2 == 6:
+    print("0+6 = 6")
+if num1 == 0 and sign == '+' and num2 == 7:
+    print("0+7 = 7")
+if num1 == 0 and sign == '+' and num2 == 8:
+    print("0+8 = 8")
+if num1 == 0 and sign == '+' and num2 == 9:
+    print("0+9 = 9")
+if num1 == 0 and sign == '+' and num2 == 10:
+    print("0+10 = 10")
+if num1 == 1 and sign == '+' and num2 == 0:
+    print("1+0 = 1")
+if num1 == 1 and sign == '+' and num2 == 1:
+    print("1+1 = 2")
+if num1 == 1 and sign == '+' and num2 == 2:
+    print("1+2 = 3")
+if num1 == 1 and sign == '+' and num2 == 3:
+    print("1+3 = 4")
+if num1 == 1 and sign == '+' and num2 == 4:
+    print("1+4 = 5")
+if num1 == 1 and sign == '+' and num2 == 5:
+    print("1+5 = 6")
+if num1 == 1 and sign == '+' and num2 == 6:
+    print("1+6 = 7")
+if num1 == 1 and sign == '+' and num2 == 7:
+    print("1+7 = 8")
+if num1 == 1 and sign == '+' and num2 == 8:
+    print("1+8 = 9")
+if num1 == 1 and sign == '+' and num2 == 9:
+    print("1+9 = 10")
+if num1 == 1 and sign == '+' and num2 == 10:
+    print("1+10 = 11")
+if num1 == 2 and sign == '+' and num2 == 0:
+    print("2+0 = 2")
+if num1 == 2 and sign == '+' and num2 == 1:
+    print("2+1 = 3")
+if num1 == 2 and sign == '+' and num2 == 2:
+    print("2+2 = 4")
+if num1 == 2 and sign == '+' and num2 == 3:
+    print("2+3 = 5")
+if num1 == 2 and sign == '+' and num2 == 4:
+    print("2+4 = 6")
+if num1 == 2 and sign == '+' and num2 == 5:
+    print("2+5 = 7")
+if num1 == 2 and sign == '+' and num2 == 6:
+    print("2+6 = 8")
+if num1 == 2 and sign == '+' and num2 == 7:
+    print("2+7 = 9")
+if num1 == 2 and sign == '+' and num2 == 8:
+    print("2+8 = 10")
+if num1 == 2 and sign == '+' and num2 == 9:
+    print("2+9 = 11")
+if num1 == 2 and sign == '+' and num2 == 10:
+    print("2+10 = 12")
+if num1 == 3 and sign == '+' and num2 == 0:
+    print("3+0 = 3")
+if num1 == 3 and sign == '+' and num2 == 1:
+    print("3+1 = 4")
+if num1 == 3 and sign == '+' and num2 == 2:
+    print("3+2 = 5")
+if num1 == 3 and sign == '+' and num2 == 3:
+    print("3+3 = 6")
+if num1 == 3 and sign == '+' and num2 == 4:
+    print("3+4 = 7")
+if num1 == 3 and sign == '+' and num2 == 5:
+    print("3+5 = 8")
+if num1 == 3 and sign == '+' and num2 == 6:
+    print("3+6 = 9")
+if num1 == 3 and sign == '+' and num2 == 7:
+    print("3+7 = 10")
+if num1 == 3 and sign == '+' and num2 == 8:
+    print("3+8 = 11")
+if num1 == 3 and sign == '+' and num2 == 9:
+    print("3+9 = 12")
+if num1 == 3 and sign == '+' and num2 == 10:
+    print("3+10 = 13")
+if num1 == 4 and sign == '+' and num2 == 0:
+    print("4+0 = 4")
+if num1 == 4 and sign == '+' and num2 == 1:
+    print("4+1 = 5")
+if num1 == 4 and sign == '+' and num2 == 2:
+    print("4+2 = 6")
+if num1 == 4 and sign == '+' and num2 == 3:
+    print("4+3 = 7")
+if num1 == 4 and sign == '+' and num2 == 4:
+    print("4+4 = 8")
+if num1 == 4 and sign == '+' and num2 == 5:
+    print("4+5 = 9")
+if num1 == 4 and sign == '+' and num2 == 6:
+    print("4+6 = 10")
+if num1 == 4 and sign == '+' and num2 == 7:
+    print("4+7 = 11")
+if num1 == 4 and sign == '+' and num2 == 8:
+    print("4+8 = 12")
+if num1 == 4 and sign == '+' and num2 == 9:
+    print("4+9 = 13")
+if num1 == 4 and sign == '+' and num2 == 10:
+    print("4+10 = 14")
+if num1 == 5 and sign == '+' and num2 == 0:
+    print("5+0 = 5")
+if num1 == 5 and sign == '+' and num2 == 1:
+    print("5+1 = 6")
+if num1 == 5 and sign == '+' and num2 == 2:
+    print("5+2 = 7")
+if num1 == 5 and sign == '+' and num2 == 3:
+    print("5+3 = 8")
+if num1 == 5 and sign == '+' and num2 == 4:
+    print("5+4 = 9")
+if num1 == 5 and sign == '+' and num2 == 5:
+    print("5+5 = 10")
+if num1 == 5 and sign == '+' and num2 == 6:
+    print("5+6 = 11")
+if num1 == 5 and sign == '+' and num2 == 7:
+    print("5+7 = 12")
+if num1 == 5 and sign == '+' and num2 == 8:
+    print("5+8 = 13")
+if num1 == 5 and sign == '+' and num2 == 9:
+    print("5+9 = 14")
+if num1 == 5 and sign == '+' and num2 == 10:
+    print("5+10 = 15")
+if num1 == 6 and sign == '+' and num2 == 0:
+    print("6+0 = 6")
+if num1 == 6 and sign == '+' and num2 == 1:
+    print("6+1 = 7")
+if num1 == 6 and sign == '+' and num2 == 2:
+    print("6+2 = 8")
+if num1 == 6 and sign == '+' and num2 == 3:
+    print("6+3 = 9")
+if num1 == 6 and sign == '+' and num2 == 4:
+    print("6+4 = 10")
+if num1 == 6 and sign == '+' and num2 == 5:
+    print("6+5 = 11")
+if num1 == 6 and sign == '+' and num2 == 6:
+    print("6+6 = 12")
+if num1 == 6 and sign == '+' and num2 == 7:
+    print("6+7 = 13")
+if num1 == 6 and sign == '+' and num2 == 8:
+    print("6+8 = 14")
+if num1 == 6 and sign == '+' and num2 == 9:
+    print("6+9 = 15")
+if num1 == 6 and sign == '+' and num2 == 10:
+    print("6+10 = 16")
+if num1 == 7 and sign == '+' and num2 == 0:
+    print("7+0 = 7")
+if num1 == 7 and sign == '+' and num2 == 1:
+    print("7+1 = 8")
+if num1 == 7 and sign == '+' and num2 == 2:
+    print("7+2 = 9")
+if num1 == 7 and sign == '+' and num2 == 3:
+    print("7+3 = 10")
+if num1 == 7 and sign == '+' and num2 == 4:
+    print("7+4 = 11")
+if num1 == 7 and sign == '+' and num2 == 5:
+    print("7+5 = 12")
+if num1 == 7 and sign == '+' and num2 == 6:
+    print("7+6 = 13")
+if num1 == 7 and sign == '+' and num2 == 7:
+    print("7+7 = 14")
+if num1 == 7 and sign == '+' and num2 == 8:
+    print("7+8 = 15")
+if num1 == 7 and sign == '+' and num2 == 9:
+    print("7+9 = 16")
+if num1 == 7 and sign == '+' and num2 == 10:
+    print("7+10 = 17")
+if num1 == 8 and sign == '+' and num2 == 0:
+    print("8+0 = 8")
+if num1 == 8 and sign == '+' and num2 == 1:
+    print("8+1 = 9")
+if num1 == 8 and sign == '+' and num2 == 2:
+    print("8+2 = 10")
+if num1 == 8 and sign == '+' and num2 == 3:
+    print("8+3 = 11")
+if num1 == 8 and sign == '+' and num2 == 4:
+    print("8+4 = 12")
+if num1 == 8 and sign == '+' and num2 == 5:
+    print("8+5 = 13")
+if num1 == 8 and sign == '+' and num2 == 6:
+    print("8+6 = 14")
+if num1 == 8 and sign == '+' and num2 == 7:
+    print("8+7 = 15")
+if num1 == 8 and sign == '+' and num2 == 8:
+    print("8+8 = 16")
+if num1 == 8 and sign == '+' and num2 == 9:
+    print("8+9 = 17")
+if num1 == 8 and sign == '+' and num2 == 10:
+    print("8+10 = 18")
+if num1 == 9 and sign == '+' and num2 == 0:
+    print("9+0 = 9")
+if num1 == 9 and sign == '+' and num2 == 1:
+    print("9+1 = 10")
+if num1 == 9 and sign == '+' and num2 == 2:
+    print("9+2 = 11")
+if num1 == 9 and sign == '+' and num2 == 3:
+    print("9+3 = 12")
+if num1 == 9 and sign == '+' and num2 == 4:
+    print("9+4 = 13")
+if num1 == 9 and sign == '+' and num2 == 5:
+    print("9+5 = 14")
+if num1 == 9 and sign == '+' and num2 == 6:
+    print("9+6 = 15")
+if num1 == 9 and sign == '+' and num2 == 7:
+    print("9+7 = 16")
+if num1 == 9 and sign == '+' and num2 == 8:
+    print("9+8 = 17")
+if num1 == 9 and sign == '+' and num2 == 9:
+    print("9+9 = 18")
+if num1 == 9 and sign == '+' and num2 == 10:
+    print("9+10 = 19")
+if num1 == 10 and sign == '+' and num2 == 0:
+    print("10+0 = 10")
+if num1 == 10 and sign == '+' and num2 == 1:
+    print("10+1 = 11")
+if num1 == 10 and sign == '+' and num2 == 2:
+    print("10+2 = 12")
+if num1 == 10 and sign == '+' and num2 == 3:
+    print("10+3 = 13")
+if num1 == 10 and sign == '+' and num2 == 4:
+    print("10+4 = 14")
+if num1 == 10 and sign == '+' and num2 == 5:
+    print("10+5 = 15")
+if num1 == 10 and sign == '+' and num2 == 6:
+    print("10+6 = 16")
+if num1 == 10 and sign == '+' and num2 == 7:
+    print("10+7 = 17")
+if num1 == 10 and sign == '+' and num2 == 8:
+    print("10+8 = 18")
+if num1 == 10 and sign == '+' and num2 == 9:
+    print("10+9 = 19")
+if num1 == 10 and sign == '+' and num2 == 10:
+    print("10+10 = 20")
