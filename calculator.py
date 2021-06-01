38f268bc2512bac63cb5d25286ec5155aa4059c3
unknown <dawid.surma@britenet.com.pl>
2021-06-01 09:26:35 +0200

    Add-Divide-Function

--- calculator.py
+++ calculator.py
@@ -488,3 +488,245 @@ if num1 == 10 and sign == '-' and num2 == 9:
     print("10-9 = 1")
 if num1 == 10 and sign == '-' and num2 == 10:
     print("10-10 = 0")
+if num1 == 0 and sign == '/' and num2 == 0:
+    print("0/0 = Undefined")
+if num1 == 0 and sign == '/' and num2 == 1:
+    print("0/1 = 0")
+if num1 == 0 and sign == '/' and num2 == 2:
+    print("0/2 = 0")
+if num1 == 0 and sign == '/' and num2 == 3:
+    print("0/3 = 0")
+if num1 == 0 and sign == '/' and num2 == 4:
+    print("0/4 = 0")
+if num1 == 0 and sign == '/' and num2 == 5:
+    print("0/5 = 0")
+if num1 == 0 and sign == '/' and num2 == 6:
+    print("0/6 = 0")
+if num1 == 0 and sign == '/' and num2 == 7:
+    print("0/7 = 0")
+if num1 == 0 and sign == '/' and num2 == 8:
+    print("0/8 = 0")
+if num1 == 0 and sign == '/' and num2 == 9:
+    print("0/9 = 0")
+if num1 == 0 and sign == '/' and num2 == 10:
+    print("0/10 = 0")
+if num1 == 1 and sign == '/' and num2 == 0:
+    print("1/0 = Inf")
+if num1 == 1 and sign == '/' and num2 == 1:
+    print("1/1 = 1")
+if num1 == 1 and sign == '/' and num2 == 2:
+    print("1/2 = 0.5")
+if num1 == 1 and sign == '/' and num2 == 3:
+    print("1/3 = 0.3333333333333333333333333333")
+if num1 == 1 and sign == '/' and num2 == 4:
+    print("1/4 = 0.25")
+if num1 == 1 and sign == '/' and num2 == 5:
+    print("1/5 = 0.2")
+if num1 == 1 and sign == '/' and num2 == 6:
+    print("1/6 = 0.1666666666666666666666666667")
+if num1 == 1 and sign == '/' and num2 == 7:
+    print("1/7 = 0.1428571428571428571428571429")
+if num1 == 1 and sign == '/' and num2 == 8:
+    print("1/8 = 0.125")
+if num1 == 1 and sign == '/' and num2 == 9:
+    print("1/9 = 0.1111111111111111111111111111")
+if num1 == 1 and sign == '/' and num2 == 10:
+    print("1/10 = 0.1")
+if num1 == 2 and sign == '/' and num2 == 0:
+    print("2/0 = Inf")
+if num1 == 2 and sign == '/' and num2 == 1:
+    print("2/1 = 2")
+if num1 == 2 and sign == '/' and num2 == 2:
+    print("2/2 = 1")
+if num1 == 2 and sign == '/' and num2 == 3:
+    print("2/3 = 0.6666666666666666666666666667")
+if num1 == 2 and sign == '/' and num2 == 4:
+    print("2/4 = 0.5")
+if num1 == 2 and sign == '/' and num2 == 5:
+    print("2/5 = 0.4")
+if num1 == 2 and sign == '/' and num2 == 6:
+    print("2/6 = 0.3333333333333333333333333333")
+if num1 == 2 and sign == '/' and num2 == 7:
+    print("2/7 = 0.2857142857142857142857142857")
+if num1 == 2 and sign == '/' and num2 == 8:
+    print("2/8 = 0.25")
+if num1 == 2 and sign == '/' and num2 == 9:
+    print("2/9 = 0.2222222222222222222222222222")
+if num1 == 2 and sign == '/' and num2 == 10:
+    print("2/10 = 0.2")
+if num1 == 3 and sign == '/' and num2 == 0:
+    print("3/0 = Inf")
+if num1 == 3 and sign == '/' and num2 == 1:
+    print("3/1 = 3")
+if num1 == 3 and sign == '/' and num2 == 2:
+    print("3/2 = 1.5")
+if num1 == 3 and sign == '/' and num2 == 3:
+    print("3/3 = 1")
+if num1 == 3 and sign == '/' and num2 == 4:
+    print("3/4 = 0.75")
+if num1 == 3 and sign == '/' and num2 == 5:
+    print("3/5 = 0.6")
+if num1 == 3 and sign == '/' and num2 == 6:
+    print("3/6 = 0.5")
+if num1 == 3 and sign == '/' and num2 == 7:
+    print("3/7 = 0.4285714285714285714285714286")
+if num1 == 3 and sign == '/' and num2 == 8:
+    print("3/8 = 0.375")
+if num1 == 3 and sign == '/' and num2 == 9:
+    print("3/9 = 0.3333333333333333333333333333")
+if num1 == 3 and sign == '/' and num2 == 10:
+    print("3/10 = 0.3")
+if num1 == 4 and sign == '/' and num2 == 0:
+    print("4/0 = Inf")
+if num1 == 4 and sign == '/' and num2 == 1:
+    print("4/1 = 4")
+if num1 == 4 and sign == '/' and num2 == 2:
+    print("4/2 = 2")
+if num1 == 4 and sign == '/' and num2 == 3:
+    print("4/3 = 1.333333333333333333333333333")
+if num1 == 4 and sign == '/' and num2 == 4:
+    print("4/4 = 1")
+if num1 == 4 and sign == '/' and num2 == 5:
+    print("4/5 = 0.8")
+if num1 == 4 and sign == '/' and num2 == 6:
+    print("4/6 = 0.6666666666666666666666666667")
+if num1 == 4 and sign == '/' and num2 == 7:
+    print("4/7 = 0.5714285714285714285714285714")
+if num1 == 4 and sign == '/' and num2 == 8:
+    print("4/8 = 0.5")
+if num1 == 4 and sign == '/' and num2 == 9:
+    print("4/9 = 0.4444444444444444444444444444")
+if num1 == 4 and sign == '/' and num2 == 10:
+    print("4/10 = 0.4")
+if num1 == 5 and sign == '/' and num2 == 0:
+    print("5/0 = Inf")
+if num1 == 5 and sign == '/' and num2 == 1:
+    print("5/1 = 5")
+if num1 == 5 and sign == '/' and num2 == 2:
+    print("5/2 = 2.5")
+if num1 == 5 and sign == '/' and num2 == 3:
+    print("5/3 = 1.666666666666666666666666667")
+if num1 == 5 and sign == '/' and num2 == 4:
+    print("5/4 = 1.25")
+if num1 == 5 and sign == '/' and num2 == 5:
+    print("5/5 = 1")
+if num1 == 5 and sign == '/' and num2 == 6:
+    print("5/6 = 0.8333333333333333333333333333")
+if num1 == 5 and sign == '/' and num2 == 7:
+    print("5/7 = 0.7142857142857142857142857143")
+if num1 == 5 and sign == '/' and num2 == 8:
+    print("5/8 = 0.625")
+if num1 == 5 and sign == '/' and num2 == 9:
+    print("5/9 = 0.5555555555555555555555555556")
+if num1 == 5 and sign == '/' and num2 == 10:
+    print("5/10 = 0.5")
+if num1 == 6 and sign == '/' and num2 == 0:
+    print("6/0 = Inf")
+if num1 == 6 and sign == '/' and num2 == 1:
+    print("6/1 = 6")
+if num1 == 6 and sign == '/' and num2 == 2:
+    print("6/2 = 3")
+if num1 == 6 and sign == '/' and num2 == 3:
+    print("6/3 = 2")
+if num1 == 6 and sign == '/' and num2 == 4:
+    print("6/4 = 1.5")
+if num1 == 6 and sign == '/' and num2 == 5:
+    print("6/5 = 1.2")
+if num1 == 6 and sign == '/' and num2 == 6:
+    print("6/6 = 1")
+if num1 == 6 and sign == '/' and num2 == 7:
+    print("6/7 = 0.8571428571428571428571428571")
+if num1 == 6 and sign == '/' and num2 == 8:
+    print("6/8 = 0.75")
+if num1 == 6 and sign == '/' and num2 == 9:
+    print("6/9 = 0.6666666666666666666666666667")
+if num1 == 6 and sign == '/' and num2 == 10:
+    print("6/10 = 0.6")
+if num1 == 7 and sign == '/' and num2 == 0:
+    print("7/0 = Inf")
+if num1 == 7 and sign == '/' and num2 == 1:
+    print("7/1 = 7")
+if num1 == 7 and sign == '/' and num2 == 2:
+    print("7/2 = 3.5")
+if num1 == 7 and sign == '/' and num2 == 3:
+    print("7/3 = 2.333333333333333333333333333")
+if num1 == 7 and sign == '/' and num2 == 4:
+    print("7/4 = 1.75")
+if num1 == 7 and sign == '/' and num2 == 5:
+    print("7/5 = 1.4")
+if num1 == 7 and sign == '/' and num2 == 6:
+    print("7/6 = 1.166666666666666666666666667")
+if num1 == 7 and sign == '/' and num2 == 7:
+    print("7/7 = 1")
+if num1 == 7 and sign == '/' and num2 == 8:
+    print("7/8 = 0.875")
+if num1 == 7 and sign == '/' and num2 == 9:
+    print("7/9 = 0.7777777777777777777777777778")
+if num1 == 7 and sign == '/' and num2 == 10:
+    print("7/10 = 0.7")
+if num1 == 8 and sign == '/' and num2 == 0:
+    print("8/0 = Inf")
+if num1 == 8 and sign == '/' and num2 == 1:
+    print("8/1 = 8")
+if num1 == 8 and sign == '/' and num2 == 2:
+    print("8/2 = 4")
+if num1 == 8 and sign == '/' and num2 == 3:
+    print("8/3 = 2.666666666666666666666666667")
+if num1 == 8 and sign == '/' and num2 == 4:
+    print("8/4 = 2")
+if num1 == 8 and sign == '/' and num2 == 5:
+    print("8/5 = 1.6")
+if num1 == 8 and sign == '/' and num2 == 6:
+    print("8/6 = 1.333333333333333333333333333")
+if num1 == 8 and sign == '/' and num2 == 7:
+    print("8/7 = 1.142857142857142857142857143")
+if num1 == 8 and sign == '/' and num2 == 8:
+    print("8/8 = 1")
+if num1 == 8 and sign == '/' and num2 == 9:
+    print("8/9 = 0.8888888888888888888888888889")
+if num1 == 8 and sign == '/' and num2 == 10:
+    print("8/10 = 0.8")
+if num1 == 9 and sign == '/' and num2 == 0:
+    print("9/0 = Inf")
+if num1 == 9 and sign == '/' and num2 == 1:
+    print("9/1 = 9")
+if num1 == 9 and sign == '/' and num2 == 2:
+    print("9/2 = 4.5")
+if num1 == 9 and sign == '/' and num2 == 3:
+    print("9/3 = 3")
+if num1 == 9 and sign == '/' and num2 == 4:
+    print("9/4 = 2.25")
+if num1 == 9 and sign == '/' and num2 == 5:
+    print("9/5 = 1.8")
+if num1 == 9 and sign == '/' and num2 == 6:
+    print("9/6 = 1.5")
+if num1 == 9 and sign == '/' and num2 == 7:
+    print("9/7 = 1.285714285714285714285714286")
+if num1 == 9 and sign == '/' and num2 == 8:
+    print("9/8 = 1.125")
+if num1 == 9 and sign == '/' and num2 == 9:
+    print("9/9 = 1")
+if num1 == 9 and sign == '/' and num2 == 10:
+    print("9/10 = 0.9")
+if num1 == 10 and sign == '/' and num2 == 0:
+    print("10/0 = Inf")
+if num1 == 10 and sign == '/' and num2 == 1:
+    print("10/1 = 10")
+if num1 == 10 and sign == '/' and num2 == 2:
+    print("10/2 = 5")
+if num1 == 10 and sign == '/' and num2 == 3:
+    print("10/3 = 3.333333333333333333333333333")
+if num1 == 10 and sign == '/' and num2 == 4:
+    print("10/4 = 2.5")
+if num1 == 10 and sign == '/' and num2 == 5:
+    print("10/5 = 2")
+if num1 == 10 and sign == '/' and num2 == 6:
+    print("10/6 = 1.666666666666666666666666667")
+if num1 == 10 and sign == '/' and num2 == 7:
+    print("10/7 = 1.428571428571428571428571429")
+if num1 == 10 and sign == '/' and num2 == 8:
+    print("10/8 = 1.25")
+if num1 == 10 and sign == '/' and num2 == 9:
+    print("10/9 = 1.111111111111111111111111111")
+if num1 == 10 and sign == '/' and num2 == 10:
+    print("10/10 = 1")
