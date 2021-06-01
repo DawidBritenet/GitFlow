3ec85a76cfdeb66020cc7759693639ba1bdde18c
unknown <dawid.surma@britenet.com.pl>
2021-06-01 09:27:27 +0200

    Add-Multiply-Function

--- calculator.py
+++ calculator.py
@@ -730,3 +730,245 @@ if num1 == 10 and sign == '/' and num2 == 9:
     print("10/9 = 1.111111111111111111111111111")
 if num1 == 10 and sign == '/' and num2 == 10:
     print("10/10 = 1")
+if num1 == 0 and sign == '*' and num2 == 0:
+    print("0*0 = 0")
+if num1 == 0 and sign == '*' and num2 == 1:
+    print("0*1 = 0")
+if num1 == 0 and sign == '*' and num2 == 2:
+    print("0*2 = 0")
+if num1 == 0 and sign == '*' and num2 == 3:
+    print("0*3 = 0")
+if num1 == 0 and sign == '*' and num2 == 4:
+    print("0*4 = 0")
+if num1 == 0 and sign == '*' and num2 == 5:
+    print("0*5 = 0")
+if num1 == 0 and sign == '*' and num2 == 6:
+    print("0*6 = 0")
+if num1 == 0 and sign == '*' and num2 == 7:
+    print("0*7 = 0")
+if num1 == 0 and sign == '*' and num2 == 8:
+    print("0*8 = 0")
+if num1 == 0 and sign == '*' and num2 == 9:
+    print("0*9 = 0")
+if num1 == 0 and sign == '*' and num2 == 10:
+    print("0*10 = 0")
+if num1 == 1 and sign == '*' and num2 == 0:
+    print("1*0 = 0")
+if num1 == 1 and sign == '*' and num2 == 1:
+    print("1*1 = 1")
+if num1 == 1 and sign == '*' and num2 == 2:
+    print("1*2 = 2")
+if num1 == 1 and sign == '*' and num2 == 3:
+    print("1*3 = 3")
+if num1 == 1 and sign == '*' and num2 == 4:
+    print("1*4 = 4")
+if num1 == 1 and sign == '*' and num2 == 5:
+    print("1*5 = 5")
+if num1 == 1 and sign == '*' and num2 == 6:
+    print("1*6 = 6")
+if num1 == 1 and sign == '*' and num2 == 7:
+    print("1*7 = 7")
+if num1 == 1 and sign == '*' and num2 == 8:
+    print("1*8 = 8")
+if num1 == 1 and sign == '*' and num2 == 9:
+    print("1*9 = 9")
+if num1 == 1 and sign == '*' and num2 == 10:
+    print("1*10 = 10")
+if num1 == 2 and sign == '*' and num2 == 0:
+    print("2*0 = 0")
+if num1 == 2 and sign == '*' and num2 == 1:
+    print("2*1 = 2")
+if num1 == 2 and sign == '*' and num2 == 2:
+    print("2*2 = 4")
+if num1 == 2 and sign == '*' and num2 == 3:
+    print("2*3 = 6")
+if num1 == 2 and sign == '*' and num2 == 4:
+    print("2*4 = 8")
+if num1 == 2 and sign == '*' and num2 == 5:
+    print("2*5 = 10")
+if num1 == 2 and sign == '*' and num2 == 6:
+    print("2*6 = 12")
+if num1 == 2 and sign == '*' and num2 == 7:
+    print("2*7 = 14")
+if num1 == 2 and sign == '*' and num2 == 8:
+    print("2*8 = 16")
+if num1 == 2 and sign == '*' and num2 == 9:
+    print("2*9 = 18")
+if num1 == 2 and sign == '*' and num2 == 10:
+    print("2*10 = 20")
+if num1 == 3 and sign == '*' and num2 == 0:
+    print("3*0 = 0")
+if num1 == 3 and sign == '*' and num2 == 1:
+    print("3*1 = 3")
+if num1 == 3 and sign == '*' and num2 == 2:
+    print("3*2 = 6")
+if num1 == 3 and sign == '*' and num2 == 3:
+    print("3*3 = 9")
+if num1 == 3 and sign == '*' and num2 == 4:
+    print("3*4 = 12")
+if num1 == 3 and sign == '*' and num2 == 5:
+    print("3*5 = 15")
+if num1 == 3 and sign == '*' and num2 == 6:
+    print("3*6 = 18")
+if num1 == 3 and sign == '*' and num2 == 7:
+    print("3*7 = 21")
+if num1 == 3 and sign == '*' and num2 == 8:
+    print("3*8 = 24")
+if num1 == 3 and sign == '*' and num2 == 9:
+    print("3*9 = 27")
+if num1 == 3 and sign == '*' and num2 == 10:
+    print("3*10 = 30")
+if num1 == 4 and sign == '*' and num2 == 0:
+    print("4*0 = 0")
+if num1 == 4 and sign == '*' and num2 == 1:
+    print("4*1 = 4")
+if num1 == 4 and sign == '*' and num2 == 2:
+    print("4*2 = 8")
+if num1 == 4 and sign == '*' and num2 == 3:
+    print("4*3 = 12")
+if num1 == 4 and sign == '*' and num2 == 4:
+    print("4*4 = 16")
+if num1 == 4 and sign == '*' and num2 == 5:
+    print("4*5 = 20")
+if num1 == 4 and sign == '*' and num2 == 6:
+    print("4*6 = 24")
+if num1 == 4 and sign == '*' and num2 == 7:
+    print("4*7 = 28")
+if num1 == 4 and sign == '*' and num2 == 8:
+    print("4*8 = 32")
+if num1 == 4 and sign == '*' and num2 == 9:
+    print("4*9 = 36")
+if num1 == 4 and sign == '*' and num2 == 10:
+    print("4*10 = 40")
+if num1 == 5 and sign == '*' and num2 == 0:
+    print("5*0 = 0")
+if num1 == 5 and sign == '*' and num2 == 1:
+    print("5*1 = 5")
+if num1 == 5 and sign == '*' and num2 == 2:
+    print("5*2 = 10")
+if num1 == 5 and sign == '*' and num2 == 3:
+    print("5*3 = 15")
+if num1 == 5 and sign == '*' and num2 == 4:
+    print("5*4 = 20")
+if num1 == 5 and sign == '*' and num2 == 5:
+    print("5*5 = 25")
+if num1 == 5 and sign == '*' and num2 == 6:
+    print("5*6 = 30")
+if num1 == 5 and sign == '*' and num2 == 7:
+    print("5*7 = 35")
+if num1 == 5 and sign == '*' and num2 == 8:
+    print("5*8 = 40")
+if num1 == 5 and sign == '*' and num2 == 9:
+    print("5*9 = 45")
+if num1 == 5 and sign == '*' and num2 == 10:
+    print("5*10 = 50")
+if num1 == 6 and sign == '*' and num2 == 0:
+    print("6*0 = 0")
+if num1 == 6 and sign == '*' and num2 == 1:
+    print("6*1 = 6")
+if num1 == 6 and sign == '*' and num2 == 2:
+    print("6*2 = 12")
+if num1 == 6 and sign == '*' and num2 == 3:
+    print("6*3 = 18")
+if num1 == 6 and sign == '*' and num2 == 4:
+    print("6*4 = 24")
+if num1 == 6 and sign == '*' and num2 == 5:
+    print("6*5 = 30")
+if num1 == 6 and sign == '*' and num2 == 6:
+    print("6*6 = 36")
+if num1 == 6 and sign == '*' and num2 == 7:
+    print("6*7 = 42")
+if num1 == 6 and sign == '*' and num2 == 8:
+    print("6*8 = 48")
+if num1 == 6 and sign == '*' and num2 == 9:
+    print("6*9 = 54")
+if num1 == 6 and sign == '*' and num2 == 10:
+    print("6*10 = 60")
+if num1 == 7 and sign == '*' and num2 == 0:
+    print("7*0 = 0")
+if num1 == 7 and sign == '*' and num2 == 1:
+    print("7*1 = 7")
+if num1 == 7 and sign == '*' and num2 == 2:
+    print("7*2 = 14")
+if num1 == 7 and sign == '*' and num2 == 3:
+    print("7*3 = 21")
+if num1 == 7 and sign == '*' and num2 == 4:
+    print("7*4 = 28")
+if num1 == 7 and sign == '*' and num2 == 5:
+    print("7*5 = 35")
+if num1 == 7 and sign == '*' and num2 == 6:
+    print("7*6 = 42")
+if num1 == 7 and sign == '*' and num2 == 7:
+    print("7*7 = 49")
+if num1 == 7 and sign == '*' and num2 == 8:
+    print("7*8 = 56")
+if num1 == 7 and sign == '*' and num2 == 9:
+    print("7*9 = 63")
+if num1 == 7 and sign == '*' and num2 == 10:
+    print("7*10 = 70")
+if num1 == 8 and sign == '*' and num2 == 0:
+    print("8*0 = 0")
+if num1 == 8 and sign == '*' and num2 == 1:
+    print("8*1 = 8")
+if num1 == 8 and sign == '*' and num2 == 2:
+    print("8*2 = 16")
+if num1 == 8 and sign == '*' and num2 == 3:
+    print("8*3 = 24")
+if num1 == 8 and sign == '*' and num2 == 4:
+    print("8*4 = 32")
+if num1 == 8 and sign == '*' and num2 == 5:
+    print("8*5 = 40")
+if num1 == 8 and sign == '*' and num2 == 6:
+    print("8*6 = 48")
+if num1 == 8 and sign == '*' and num2 == 7:
+    print("8*7 = 56")
+if num1 == 8 and sign == '*' and num2 == 8:
+    print("8*8 = 64")
+if num1 == 8 and sign == '*' and num2 == 9:
+    print("8*9 = 72")
+if num1 == 8 and sign == '*' and num2 == 10:
+    print("8*10 = 80")
+if num1 == 9 and sign == '*' and num2 == 0:
+    print("9*0 = 0")
+if num1 == 9 and sign == '*' and num2 == 1:
+    print("9*1 = 9")
+if num1 == 9 and sign == '*' and num2 == 2:
+    print("9*2 = 18")
+if num1 == 9 and sign == '*' and num2 == 3:
+    print("9*3 = 27")
+if num1 == 9 and sign == '*' and num2 == 4:
+    print("9*4 = 36")
+if num1 == 9 and sign == '*' and num2 == 5:
+    print("9*5 = 45")
+if num1 == 9 and sign == '*' and num2 == 6:
+    print("9*6 = 54")
+if num1 == 9 and sign == '*' and num2 == 7:
+    print("9*7 = 63")
+if num1 == 9 and sign == '*' and num2 == 8:
+    print("9*8 = 72")
+if num1 == 9 and sign == '*' and num2 == 9:
+    print("9*9 = 81")
+if num1 == 9 and sign == '*' and num2 == 10:
+    print("9*10 = 90")
+if num1 == 10 and sign == '*' and num2 == 0:
+    print("10*0 = 0")
+if num1 == 10 and sign == '*' and num2 == 1:
+    print("10*1 = 10")
+if num1 == 10 and sign == '*' and num2 == 2:
+    print("10*2 = 20")
+if num1 == 10 and sign == '*' and num2 == 3:
+    print("10*3 = 30")
+if num1 == 10 and sign == '*' and num2 == 4:
+    print("10*4 = 40")
+if num1 == 10 and sign == '*' and num2 == 5:
+    print("10*5 = 50")
+if num1 == 10 and sign == '*' and num2 == 6:
+    print("10*6 = 60")
+if num1 == 10 and sign == '*' and num2 == 7:
+    print("10*7 = 70")
+if num1 == 10 and sign == '*' and num2 == 8:
+    print("10*8 = 80")
+if num1 == 10 and sign == '*' and num2 == 9:
+    print("10*9 = 90")
+if num1 == 10 and sign == '*' and num2 == 10:
+    print("10*10 = 100")
